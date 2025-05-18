6d81f16d588465f181524a1edc79e4148d0ab77a
diegonola <nolasco.advocacia@hotmail.com>
2025-05-18 13:03:28 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,10 @@ int main() {
     float pib1= 699.28;
     int pontos_turisticos1= 50;
 
+    //avenureiro
+    float densidade1 = (float) populacao1 / area1;
+    float pib_c1 = (float) pib1 / populacao1;
+
 //carta_2
     char estado2= 'B';
     char carta2[15]= "B01";
@@ -19,16 +23,25 @@ int main() {
     float area2= 3151.00;
     float pib2= 169.18;
     int pontos_turisticos2= 10;
+
+    //avenureiro
+    float densidade2 = (float)populacao2 / area2;
+    float pib_c2 = (float) pib2 / populacao2;
            
 // Exibe cartas
-    printf("                     carta 1                -            carta 2\n");
-    printf("estados:                %c                             %c\n", estado1, estado2);
-    printf("cartas:                %s                            %s\n", carta1, carta2);
-    printf("cidade:               %s                        %s\n", cidade1, cidade2);
-    printf("populacao:              %d                    %d\n", populacao1, populacao2);
-    printf("area:                %.2f                      %.2f\n", area1, area2);
-    printf("pib:                %.2f                        %.2f\n", pib1, pib2);
-    printf("pontos turisticos:        %d                        %d\n", pontos_turisticos1, pontos_turisticos2);
+    printf("                  carta 1             -            carta 2\n");
+    printf("Est:                %c                             %c\n", estado1, estado2);
+    printf("Car:                %s                             %s\n", carta1, carta2);
+    printf("Cid:                %s                             %s\n", cidade1, cidade2);
+    printf("Pop:                %d                             %d\n", populacao1, populacao2);
+    printf("Área:               %.2f                           %.2f\n", area1, area2);
+    printf("PIB:                %.2f                           %.2f\n", pib1, pib2);
+    printf("P.T.:               %d                             %d\n", pontos_turisticos1, pontos_turisticos2);
+
+
+    //avenureiro
+    printf ("D.P.:              %.2f                          %.2f\n", densidade1, densidade2);
+    printf ("PIBpC:             %f                          %f\n", pib_c1, pib_c2 );
 
     return 0;
 
